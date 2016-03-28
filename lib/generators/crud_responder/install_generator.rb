2302f8d7ff7303e61cbea956c3f6294a2c4fc690
Evgeniy Chernyshev <EvgeniyRRU@gmail.com>
2016-03-29 00:10:12 +0200

    Make Rubocop happy

--- lib/generators/crud_responder/install_generator.rb
+++ lib/generators/crud_responder/install_generator.rb
@@ -1,19 +1,19 @@
 module CrudResponder
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("..", __FILE__)
+      source_root File.expand_path('..', __FILE__)
 
-      desc "Creates an initializer with default crud_responder configuration and copy locale file"
+      desc 'Creates an initializer with default crud_responder configuration and copy locale file'
 
       def update_application_controller
-        inject_into_class "app/controllers/application_controller.rb", "ApplicationController", <<-RUBY
+        inject_into_class 'app/controllers/application_controller.rb', 'ApplicationController', <<-RUBY
   include CrudResponder
 
         RUBY
       end
 
       def copy_locale
-        copy_file "../../crud_responder/locales/en.yml", "config/locales/crud_responder.en.yml"
+        copy_file '../../crud_responder/locales/en.yml', 'config/locales/crud_responder.en.yml'
       end
     end
   end
